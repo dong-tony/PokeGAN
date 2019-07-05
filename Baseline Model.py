d44c6f909332001c4bbc8bdf46329de489686ab1
Tony Dong <dong.tony@outlook.com>
2019-07-05 15:09:24 -0400

    autoencoder update

--- Baseline Model.py
+++ Baseline Model.py
@@ -6,13 +6,11 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 from torchvision import datasets, transforms
-
 import time
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #%%
-train_images = datasets.ImageFolder(root='C:\\Users\\nucle\\Documents\\GitHub\\PokeGAN\\Data\\resized and sorted', 
-                                           transform=transforms.ToTensor())
-
+train_images = datasets.ImageFolder(root='.\\Data\\resized and sorted', transform=transforms.ToTensor())
 #%%
 class Autoencoder(nn.Module):
     def __init__(self):
@@ -32,8 +30,8 @@ class Autoencoder(nn.Module):
             nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1),
             nn.Sigmoid()
         )
-
     def forward(self, x):
+        x = x.to(device)
         x = self.encoder(x)
         x = self.decoder(x)
         return x
@@ -44,14 +42,14 @@ def train(model, num_epochs=5, batch_size=64, learning_rate=1e-3):
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=learning_rate, 
                                  weight_decay=1e-5) # <--
-    train_loader = torch.utils.data.DataLoader(train_img_list, 
+    train_loader = torch.utils.data.DataLoader(train_images, 
                                                batch_size=batch_size, 
                                                shuffle=True)
     outputs = []
     for epoch in range(num_epochs):
         start_time = time.time()
         for data in train_loader:
-            img, _ = data
+            img, label = data[0].to(device), data[1].to(device)
             recon = model(img)
             loss = criterion(recon, img)
             loss.backward()
@@ -65,32 +63,16 @@ def train(model, num_epochs=5, batch_size=64, learning_rate=1e-3):
         outputs.append((epoch, img, recon),)
     return outputs
 #%%
-model = Autoencoder()
-model_underfit = Autoencoder()
-
-max_epochs = 2
-
-outputs = train(model_underfit, num_epochs=max_epochs)
+model = Autoencoder().to(device)
+outputs = train(model, num_epochs = 5)
 #%%
-sample = torch.reshape(train_img_list[0][0], (224,224,3))
-plt.imshow(sample.detach().numpy())
-#%%
-
-imgs = outputs[max_epochs-1][1].detach().numpy()
-plt.subplot(1, 2, 1)
-plt.imshow(imgs[0][0], cmap = 'CMRmap_r')
-plt.subplot(1, 2, 2)
-plt.imshow(imgs[8][0])
-
-#%%
-def interpolate(index1, index2):
-    x1 = train_images[index1][0]
-    x2 = train_images[index2][0]
+def interpolate(model, index1, index2):
+    x1 = train_images[index1][0].to(device)
+    x2 = train_images[index2][0].to(device)
     x = torch.stack([x1,x2])
-    embedding = model_underfit.encoder(x)
+    embedding = model.encoder(x)
     e1 = embedding[0] # embedding of first image
     e2 = embedding[1] # embedding of second image
-
 
     embedding_values = []
     for i in range(0, 10):
@@ -98,27 +80,20 @@ def interpolate(index1, index2):
         embedding_values.append(e)
     embedding_values = torch.stack(embedding_values)
 
-    recons = model_underfit.decoder(embedding_values)
+    recons = model.decoder(embedding_values)
 
     plt.figure(figsize=(10, 2))
-    for i, recon in enumerate(recons.detach().numpy()):
+    for i, recon in enumerate(recons.detach().cpu().numpy()):
         plt.subplot(2,10,i+1)
-        recon = np.reshape(recon, (224,224,3))
+        recon = np.moveaxis(recon,0,2)
         plt.imshow(recon)
-        
-    plt.subplot(2,10,11)
-    plt.imshow(x2[0])
-    plt.subplot(2,10,20)
-    plt.imshow(x1[0])
     
-
-interpolate(7, 2)
+    x1 = np.moveaxis(x1.cpu().numpy(),0,2)
+    x2 = np.moveaxis(x2.cpu().numpy(),0,2)
+    
+    plt.subplot(2,10,11)
+    plt.imshow(x2)
+    plt.subplot(2,10,20)
+    plt.imshow(x1)
 #%%
-interpolate(10,7)
-
-#%%
-# Run for less epochs
-# Get more data
-# Imshow colour
-
-imgs[0][0].shape
+interpolate(model,10,7)
