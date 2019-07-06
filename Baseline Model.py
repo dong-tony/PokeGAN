eacee428808d36766c6c4fef64c0c10e4cfba085
Tony Dong <dong.tony@outlook.com>
2019-07-06 14:37:27 -0400

    autoencoder update - data augmentation and training visualization

--- Baseline Model.py
+++ Baseline Model.py
@@ -8,9 +8,24 @@ import numpy as np
 from torchvision import datasets, transforms
 import time
 import matplotlib.pyplot as plt
+import random
+import torchvision
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #%%
 train_images = datasets.ImageFolder(root='.\\Data\\resized and sorted', transform=transforms.ToTensor())
+#%% data augmentation
+trans = torchvision.transforms.Compose([
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomRotation(20),
+    torchvision.transforms.ToTensor()
+])
+
+expanded_dataset = []
+for i in range(2):
+    train_images_aug = datasets.ImageFolder(root='.\\Data\\resized and sorted', transform=trans)
+    for j, item in enumerate(train_images_aug):
+        expanded_dataset.append(item)
+
 #%%
 class Autoencoder(nn.Module):
     def __init__(self):
@@ -36,18 +51,19 @@ class Autoencoder(nn.Module):
         x = self.decoder(x)
         return x
 #%%
-def train(model, num_epochs=5, batch_size=64, learning_rate=1e-3):
-    torch.manual_seed(42)
+def train(model, num_epochs=5, batch_size=64, learning_rate=1e-3, shuffle = False):
+    torch.manual_seed(360)
+    rand = random.randint(0,batch_size)
+    fig, ax = plt.subplots(1, num_epochs+1, figsize = (num_epochs+1, 1))
     criterion = nn.MSELoss() # mean square error loss
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=learning_rate, 
                                  weight_decay=1e-5) # <--
-    train_loader = torch.utils.data.DataLoader(train_images, 
+    train_loader = torch.utils.data.DataLoader(expanded_dataset, 
                                                batch_size=batch_size, 
-                                               shuffle=True)
+                                               shuffle= shuffle)
     outputs = []
     for epoch in range(num_epochs):
-        start_time = time.time()
         for data in train_loader:
             img, label = data[0].to(device), data[1].to(device)
             recon = model(img)
@@ -55,16 +71,21 @@ def train(model, num_epochs=5, batch_size=64, learning_rate=1e-3):
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-
-        end_time = time.time()
-        diff = end_time - start_time
-        print(diff)
-        print('Epoch:{}, Loss:{:.4f}'.format(epoch+1, float(loss)))
-        outputs.append((epoch, img, recon),)
+        print('Epoch:{}/{}, Loss:{:.4f}'.format(epoch+1, num_epochs, float(loss)))
+        outputs.append((epoch, img, recon))
+        if num_epochs > 1:
+            if epoch == 0:
+                ax[epoch].imshow(np.moveaxis(outputs[epoch][1][rand].detach().cpu().numpy(),0,2))
+                ax[epoch+1].imshow(np.moveaxis(outputs[epoch][2][rand].detach().cpu().numpy(),0,2))
+            else:
+                ax[epoch+1].imshow(np.moveaxis(outputs[epoch][2][rand].detach().cpu().numpy(),0,2))
+        else:
+            ax[epoch].imshow(np.moveaxis(outputs[epoch][1][rand].detach().cpu().numpy(),0,2))
+            ax[epoch+1].imshow(np.moveaxis(outputs[epoch][2][rand].detach().cpu().numpy(),0,2))
     return outputs
 #%%
 model = Autoencoder().to(device)
-outputs = train(model, num_epochs = 5)
+outputs = train(model, num_epochs = 2, shuffle = False)
 #%%
 def interpolate(model, index1, index2):
     x1 = train_images[index1][0].to(device)
@@ -96,4 +117,4 @@ def interpolate(model, index1, index2):
     plt.subplot(2,10,20)
     plt.imshow(x1)
 #%%
-interpolate(model,10,7)
+interpolate(model,7,2)
