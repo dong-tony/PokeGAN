6ec9a4d3179d60a9f52017988501c6847837fcfb
Tony Dong <dong.tony@outlook.com>
2019-07-15 09:42:08 -0400

    more checkpoints

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import torch
 from train import evaluate, train
 from network import Discriminator, Generator, init_weights
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from torchvision import datasets, transforms
 import numpy as np
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -11,10 +12,10 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 D = Discriminator().to(device)
 G = Generator().to(device)
 ### load checkpoints
-# D.load_state_dict(torch.load('./Checkpoints/0713/DWeights_500'))
-# G.load_state_dict(torch.load('./Checkpoints/0713/GWeights_500'))
-D.load_state_dict(torch.load('./Checkpoints/0713/sigmoid/DWeights_400'))
-G.load_state_dict(torch.load('./Checkpoints/0713/sigmoid/GWeights_400'))
+D.load_state_dict(torch.load('./Checkpoints/0714/DWeights_400'))
+G.load_state_dict(torch.load('./Checkpoints/0714/GWeights_400'))
+# D.load_state_dict(torch.load('./Checkpoints/0713/sigmoid/DWeights_400'))
+# G.load_state_dict(torch.load('./Checkpoints/0713/sigmoid/GWeights_400'))
 #%%
 test_noise = torch.randn(18, 110).to(device)
 eye = torch.eye(18)
@@ -25,9 +26,10 @@ G.eval()
 D.eval()
 test_images = G(test_noise).cpu()
 test_images = np.moveaxis(test_images.detach().numpy(), 1,3)
-plt.figure(figsize=(9, 3))
+plt.figure(figsize=(18, 5))
 for k in range(18):
     plt.subplot(2, 9, k+1)
+    plt.gcf().patch.set_facecolor('black')
     plt.gca().set_title('{}'.format(train_images.classes[k]))
-    plt.imshow(test_images[k])
+    plt.imshow(np.clip(test_images[k],0,1))
 plt.show()
